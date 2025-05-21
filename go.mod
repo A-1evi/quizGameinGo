b35b7b7e613b0d358847e2a94da8c3a488ca588b
Alankar <alankar010499@gmail.com>
2025-05-21 20:09:37 +0530

    Simple Quiz game with Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module quizGameinGo
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFileName))
+
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the csv file")
+	}
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, problem.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.answer {
+			correct++
+			fmt.Println("Correct!")
+
+		}
+
+	}
+	fmt.Printf("You scored %d out of %d. \n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for index, line := range lines {
+		problems[index] = problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+
+	return problems
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
